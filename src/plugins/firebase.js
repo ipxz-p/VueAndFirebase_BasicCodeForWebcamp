2566d8e9f4050a8439477c70896796e9ae79b555
ipxz-p <88420839+ipxz-p@users.noreply.github.com>
2022-06-30 23:11:36 +0700

    Update firebase.js
    remove key in database

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/firestore';
 import 'firebase/auth'
 const db = firebase.initializeApp({
-  apiKey: "AIzaSyA8AsMT6HiuoriyNm4V7LWJT-HL8p02G0E",
-  authDomain: "crud-fc706.firebaseapp.com",
-  projectId: "crud-fc706",
-  storageBucket: "crud-fc706.appspot.com",
-  messagingSenderId: "774056859388",
-  appId: "1:774056859388:web:bc757a6e5fde08ae50e979"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 // Initialize Firebase
